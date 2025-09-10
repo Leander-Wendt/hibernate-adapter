3cec741724124ad8bef70a2c8cfc73eac3b7ccc8
Leander Wendt <leander.wendt@iav.de>
2025-09-10 11:49:09 +0200

    Remove configuration of mysql dialect

--- src/main/java/org/casbin/adapter/HibernateAdapter.java
+++ src/main/java/org/casbin/adapter/HibernateAdapter.java
@@ -164,9 +164,7 @@ public class HibernateAdapter implements Adapter {
             properties.setProperty("hibernate.connection.url", this.url);
             properties.setProperty("hibernate.connection.username", this.username);
             properties.setProperty("hibernate.connection.password", this.password);
-            if (this.driver.contains("mysql")) {
-                properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
-            } else if (this.driver.contains("oracle")) {
+            if (this.driver.contains("oracle")) {
                 properties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle9iDialect");
             } else if (this.driver.contains("sqlserver")) {
                 properties.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServer2012Dialect");
